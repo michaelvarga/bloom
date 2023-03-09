b8e07e88c512e48b17b07457864ce840cfaf1762
Mike Varga <mikevarga07@gmail.com>
2023-03-09 17:16:26 -0600

    Created shopping session model (blank) and added comments for table associations

--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,27 @@ const Order = require("./models/Order");
 const Plant = require("./models/Plant");
 const Cart_Item = require("./models/Cart_Item");
 
+/*
+3 types of data
+  Static Data
+    --plants
+    --users
+    --discounts
+
+  Session Data
+    --shopping_session
+    --cart_item
+
+  Processed Data
+    --orders
+    --order_details
+    --payments
+*/
+
+// one-to-one between user, shopping_session
+// one-to-many between shopping_session, cart_items
+// one-to-one between plant and cart_item
+
 Order.belongsToMany(Plant, { through: "Order_Details" });
 Plant.belongsToMany(Order, { through: "Order_Details" });
 
