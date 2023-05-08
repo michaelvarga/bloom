fd3b5af11616f32ef9a20f32e49f8ff107e8f529
Mike Varga <mikevarga07@gmail.com>
2023-05-08 15:10:20 -0500

    Made updates to the content of the about page, including adding terms of service and about us sections

--- client/src/pages/about/About.js
+++ client/src/pages/about/About.js
@@ -1,12 +1,167 @@
-import React from 'react'
+import React from "react";
+import "./about.scss"
 
 function About() {
   return (
-    <div>About
-
-      
+    <div className="d-flex row justify-content-center">
+      <div className="about-container">
+        <h2>About Us</h2>
+        <p>
+          Welcome to Bloom, your ultimate destination for all things plants!
+        </p>
+        <p>
+          At Bloom, we believe that plants have the power to transform any
+          space, whether it’s your home, office or outdoor garden. We provide a
+          wide range of indoor and outdoor plants that are not only visually
+          appealing but also come with a host of benefits for your mental and
+          physical wellbeing.
+        </p>
+        <p>
+          Our team is dedicated to curating the finest selection of plants that
+          are both easy to care for and aesthetically pleasing. From succulents
+          and cacti to exotic orchids and towering fiddle leaf figs, we have
+          something for everyone. Whether you are a seasoned plant enthusiast or
+          just starting out, we are here to help you find the perfect plants for
+          your space.
+        </p>
+        <p>
+          We understand that purchasing plants online can be a daunting
+          experience, which is why we have made it our mission to provide a
+          seamless and hassle-free shopping experience. We ship our plants
+          nationwide, ensuring that you receive your new green friends in
+          perfect condition and ready to thrive in their new home.
+        </p>
+        <p>
+          At Bloom, we are passionate about plants and the positive impact they
+          can have on our lives. We are committed to providing the highest
+          quality products and exceptional customer service, so that you can
+          experience the joy of plant parenthood with confidence.
+        </p>
+        <p>
+          Thank you for choosing Bloom as your go-to destination for all your
+          plant needs. Let’s bring a little bit of nature into your world!
+        </p>
+      </div>
+      <div className="about-container">
+        <h2>Terms of Service Agreement</h2>
+        <p>
+          Welcome to Bloom, ("we," "us," or "our"). By using the Bloom website
+          (the "Site") or any services provided in connection with the Site (the
+          "Services"), you agree to comply with and be bound by the following
+          terms and conditions of use (the "Agreement").
+        </p>
+        <ol>
+          <li>
+            <p>Acceptance of Terms</p>
+            <p>
+              By using the Site and/or Services, you agree to be bound by this
+              Agreement, whether or not you are a registered user of our
+              Services. If you do not agree to the terms of this Agreement, you
+              may not use the Site or Services.
+            </p>
+          </li>
+          <li>
+            <p>Use of the Site and Services</p>
+            <p>
+              You agree to use the Site and Services only for lawful purposes
+              and in a manner consistent with all applicable laws and
+              regulations. You may not use the Site or Services to infringe on
+              the intellectual property rights of others or to engage in any
+              illegal activity.
+            </p>
+          </li>
+          <li>
+            <p>Orders and Payment</p>
+            <p>
+              By placing an order through the Site, you represent and warrant
+              that you are at least 18 years old and have the legal capacity to
+              enter into a binding contract. You agree to provide accurate and
+              complete information when placing an order through the Site, and
+              to promptly update any information provided if it becomes
+              inaccurate or outdated. You are responsible for all charges
+              incurred on your account, including any applicable taxes.
+            </p>
+          </li>
+          <li>
+            <p>Shipping and Returns</p>
+            <p>
+              We will ship the plants to the address you provide during
+              checkout. We will make every effort to ensure that your plants
+              arrive in good condition, but we cannot be responsible for any
+              damage or loss that occurs during shipping. If your plants arrive
+              damaged or dead, please contact us within 48 hours of delivery to
+              arrange for a replacement or refund.
+            </p>
+          </li>
+          <li>
+            <p>Intellectual Property</p>
+            <p>
+              All content on the Site, including but not limited to text,
+              graphics, logos, images, and software, is the property of Bloom or
+              its affiliates and is protected by copyright, trademark, and other
+              intellectual property laws. You may not copy, modify, distribute,
+              or otherwise use any content on the Site without our prior written
+              consent.
+            </p>
+          </li>
+          <li>
+            <p>Disclaimer of Warranties</p>
+            <p>
+              THE SITE AND SERVICES ARE PROVIDED "AS IS" AND WITHOUT WARRANTIES
+              OF ANY KIND, EITHER EXPRESS OR IMPLIED. WE DO NOT WARRANT THAT THE
+              SITE OR SERVICES WILL BE UNINTERRUPTED OR ERROR-FREE, OR THAT THE
+              SITE OR SERVICES WILL MEET YOUR REQUIREMENTS OR EXPECTATIONS.
+            </p>
+          </li>
+          <li>
+            <p>Limitation of Liability</p>
+            <p>
+              IN NO EVENT SHALL WE BE LIABLE FOR ANY INDIRECT, INCIDENTAL,
+              SPECIAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF OR IN CONNECTION
+              WITH YOUR USE OF THE SITE OR SERVICES, INCLUDING BUT NOT LIMITED
+              TO LOSS OF REVENUE OR PROFITS, LOST OR DAMAGED PLANTS, OR ANY
+              OTHER DAMAGES ARISING OUT OF OR RELATED TO YOUR USE OF THE SITE OR
+              SERVICES.
+            </p>
+          </li>
+          <li>
+            <p>Governing Law and Venue</p>
+            <p>
+              This Agreement shall be governed by and construed in accordance
+              with the laws of the state of [state], without giving effect to
+              any principles of conflicts of law. Any legal action or proceeding
+              arising out of or relating to this Agreement or your use of the
+              Site or Services shall be brought exclusively in the federal or
+              state courts located in [county, state], and you hereby consent to
+              the jurisdiction of such courts.
+            </p>
+          </li>
+          <li>
+            <p>Changes to this Agreement</p>
+            <p>
+              We reserve the right to change this Agreement at any time, and
+              your continued use of the Site or Services following any such
+              changes shall constitute your acceptance of such changes. It is
+              your responsibility to review this Agreement periodically for
+              changes.
+            </p>
+          </li>
+          <li>
+            <p>Termination</p>
+            <p>
+              We may terminate this Agreement and your access to the Site or
+              Services at any time, with or without cause, without notice or
+              liability to you.
+            </p>
+          </li>
+        </ol>
+        <p>
+          If you have any questions or concerns about this Agreement, please
+          contact us.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
